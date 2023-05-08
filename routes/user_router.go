437dc8ea9fdf249496977d6f4af0596d5d8d12ae
tole <octaadsp.camp@gmail.com>
2023-05-08 12:18:00 +0700

    User Fix Bug

--- routes/user_router.go
+++ routes/user_router.go
@@ -15,6 +15,6 @@ func UserRoutes(e *echo.Group) {
 
 	e.GET("/users", h.FindUsers)
 	e.GET("/user/:id", h.GetUser)
-	e.PATCH("/user/:id", middleware.Auth(h.UpdateUser))
+	e.PATCH("/user/:id", middleware.UploadImage(h.UpdateUser))
 	e.DELETE("/user/:id", h.DeleteUser)
 }
