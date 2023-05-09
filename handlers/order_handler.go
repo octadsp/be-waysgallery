86d4033bcbb6b6312016fc3f45fb05024cbd60e8
tole <octaadsp.camp@gmail.com>
2023-05-09 07:36:12 +0700

    ubah handler order

--- handlers/order_handler.go
+++ handlers/order_handler.go
@@ -91,8 +91,8 @@ func (h *handlerOrder) AddOrderByUserToUser(c echo.Context) error {
 	}
 
 	price, _ := strconv.Atoi(c.FormValue("price"))
-	orderTo, _ := strconv.Atoi(c.Param("toID"))
-	orderBy, _ := strconv.Atoi(c.Param("byID"))
+	orderTo, _ := strconv.Atoi(c.Param("orderTo"))
+	orderBy, _ := strconv.Atoi(c.Param("orderBy"))
 
 	order := models.Order{
 		Title:       request.Title,
